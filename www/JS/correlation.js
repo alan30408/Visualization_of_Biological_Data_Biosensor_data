6e4ac95003e37e4b4fe05ac4a6833fb175a003b0
Alan Tung <mahorox1046@gmail.com>
2021-01-10 17:20:51 +0800

    git push

--- www/JS/correlation.js
+++ www/JS/correlation.js
@@ -1,3 +1,14 @@
+dataset_variable1 = LoadBaseData("Calories", "2015-05-20", "2015-05-21")["data"];
+dataset_variable2 = LoadBaseData("HR", "2015-05-20", "2015-05-21")["data"];
+
+
+data = dataset_variable1
+for ( var i = 0; i < dataset_variable2.length; i++) {
+	data[i]["HR"] = dataset_variable2[i].HR
+}
+
+console.log(data)
+
 // set svg dimensions and margins
 const svgWidth = 500 ;
 const svgHeight = 360;
@@ -14,14 +25,10 @@ let svg = d3.select("#plot")
   .append("g")
   .attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
-console.log(dataset);
-// get the data
-data = dataset["numbers"];
-// select feature to build the histogram with
 
 // X axis: scale and draw:
 var x = d3.scaleLinear()
-	.domain([0, 100])     // d3.max(data, function(d) { return +d.price })
+	.domain([0, 1.1*d3.max(data, function(d) { return + d.Calories })])     // d3.max(data, function(d) { return +d.price })
 	.range([0, svgWidth]);
 	
 svg.append("g")
@@ -31,43 +38,38 @@ svg.append("g")
 // Y axis: scale and draw:
 var y = d3.scaleLinear()
 	.range([svgHeight, 0]);
-	y.domain([0,100]);   // d3.max(bins, function(d) { return d.length; })
+	y.domain([0, 1.1*d3.max(data, function(d) { return + d.HR })]);   // d3.max(bins, function(d) { return d.length; })
 	
 svg.append("g")
 	.call(d3.axisLeft(y));
 
 // set tooltip for mouse movement
-var tooltip = d3.select("#plot_1").append("div")
+let tooltip = d3.select("#tooltip")
 	.attr("class", "tooltip")
 	.style("opacity", 0);
 
+function mouseOver (event,d){
+	tooltip.transition()
+		.duration(200)
+		.style("opacity", .9);
+		tooltip.html("Calories:" + d.Calories + "<br>HR:" + d.HR);
+}
 
+function mouseOut (event,d){
+	tooltip.transition()
+		.duration(500)
+		.style("opacity", 0);
+}
 
 // append the bar rectangles to the svg element
-vg_1.append('g')
+svg.append('g')
 	.selectAll("dot")
-	.data(dataset)
+	.data(data)
 	.enter()
 	.append("circle")
-		.attr("class", function(d) { return d.continent} )
-		.attr("cx", function (d) { return x(d.income/1000); } )
-		.attr("cy", function (d) { return y(d.cellphone); } )
-		.attr("r", function (d) { return z(Math.sqrt(d.population)); } )
-		.style("fill", function (d) { return myColor(d.continent);} )
-		.style("opacity", "0.7")
-		.attr("stroke", "black")
-		.style("stroke-width", "1px")
-	.on("mouseover", function(event,d) {
-		tooltip.transition()
-			.duration(200)
-			.style("opacity", .9);
-			tooltip.html(d.country + "<br>Population:" + d.population + "<br>GDP:" + d.income + "<br>Cell phone:" + d.cellphone)
-			.style("left", (event.pageX) + "px")
-			.style("top", (event.pageY - 28) + "px");
-			
-		})
-	.on("mouseout", function(d) {
-		tooltip.transition()
-			.duration(500)
-			.style("opacity", 0);
-		});
+		.attr("cx", function (d) { return x(d.Calories); } )
+		.attr("cy", function (d) { return y(d.HR); } )
+		.style("fill",  "#b36969" )
+		.attr("r", 3)
+	.on("mouseover",mouseOver)
+	.on("mouseout",mouseOut);
