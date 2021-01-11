4280f351c5a321ebad298bbaeff52fc86afdb0da
Jaka Jenko <jaka.jenko@kivi.eu>
2021-01-11 12:49:34 +0100

    added GeneralData and fixed showing graph

--- www/JS/LoadingData.js
+++ www/JS/LoadingData.js
@@ -1,26 +1,26 @@
 function LoadBaseData(variables, timeIntervalStart, timeIntervalEnd)
 {
-    baseUrl = "http://127.0.0.1:5000/LoadGeneralData?";
+    var baseUrl = "http://127.0.0.1:5000/LoadGeneralData?";
 
-    variablesUrl = "variables="+variables;
-    timeIntervalStartUrl = "timeIntervalStart="+timeIntervalStart;
-    timeIntervalEndUrl = "timeIntervalEnd="+timeIntervalEnd;
+    var variablesUrl = "variables="+variables;
+    var timeIntervalStartUrl = "timeIntervalStart="+timeIntervalStart;
+    var timeIntervalEndUrl = "timeIntervalEnd="+timeIntervalEnd;
 
-    url = baseUrl + variablesUrl + "&" + timeIntervalStartUrl + "&" + timeIntervalEndUrl;
+    var url = baseUrl + variablesUrl + "&" + timeIntervalStartUrl + "&" + timeIntervalEndUrl;
 
-    data = HttpGet(url);
+    var data = HttpGet(url);
     return JSON.parse(data);
 }
 
 function LoadCorrData(variables, timeIntervalStart, timeIntervalEnd) {
-    baseUrl = "http://127.0.0.1:5000/LoadCorrelatedData?";
+    var baseUrl = "http://127.0.0.1:5000/LoadCorrelatedData?";
 
-    variablesUrl = "variables="+variables;
-    timeIntervalStartUrl = "timeIntervalStart="+timeIntervalStart;
-    timeIntervalEndUrl = "timeIntervalEnd="+timeIntervalEnd;
+    var variablesUrl = "variables="+variables;
+    var timeIntervalStartUrl = "timeIntervalStart="+timeIntervalStart;
+    var timeIntervalEndUrl = "timeIntervalEnd="+timeIntervalEnd;
 
-    url = baseUrl + variablesUrl + "&" + timeIntervalStartUrl + "&" + timeIntervalEndUrl;
+    var url = baseUrl + variablesUrl + "&" + timeIntervalStartUrl + "&" + timeIntervalEndUrl;
 
-    data = HttpGet(url);
+    var data = HttpGet(url);
     return data;
 }
